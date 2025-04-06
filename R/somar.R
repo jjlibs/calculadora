39966a05ae2351d0e33a741a08a7912208a984bf
jjlibs <jader.silvajale@ufrpe.br>
2025-04-06 17:06:56 -0300

    First commit

---
+++ R/somar.R
@@ -0,0 +1,17 @@
+#' Somar
+#'
+#' @description
+#' Esta funcao retorna a soma de dois numeros.
+#'
+#' @param x um valor numerico
+#' @param y um valor numerico
+#'
+#' @returns x + y
+#'
+#' @examples
+#' somar(1, 1)
+#'
+#' @export
+somar <- function(x, y) {
+  x + y
+}
