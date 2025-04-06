5f6e12135e2e7b9edbe1cea0c6e564aaaa078de7
jjlibs <jader.silvajale@ufrpe.br>
2025-04-06 17:15:34 -0300

    Adiciona a função subtrair.

---
+++ R/subtrair.R
@@ -0,0 +1,17 @@
+#' Subtrair
+#'
+#' @description
+#' Esta funcao retorna a subtracao de dois numeros.
+#'
+#' @param x um valor numerico
+#' @param y um valor numerico
+#'
+#' @returns x - y
+#'
+#' @examples
+#' subtrair(10, 5)
+#'
+#' @export
+subtrair <- function(x, y) {
+  x - y
+}
